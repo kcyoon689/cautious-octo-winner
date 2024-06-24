01a16e734e30c6fa2cbb67bead65fcd178a94457
kcyoon689 <kcyoun689@gmail.com>
2024-06-24 17:27:43 +0900

    apply tqdm

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import os
 import json
-from argparse import ArgumentParser
+from tqdm import tqdm
 from PIL import Image, ImageDraw
 
+image_folder_list = [
+    "data/gt_back_cropped_1/test", "data/gt_back_cropped_2/test", "data/gt_back_cropped_3/test",
+    "data/gt_back_cropped_4/test", "data/gt_back_cropped_5/test", "data/gt_back_cropped_6/test",
+    "data/gt_back_cropped_7/test", "data/gt_back_cropped_8/test", "data/gt_front_cropped_1/test",
+    "data/gt_front_cropped_2/test", "data/gt_front_cropped_3/test"
+]
 
-image_folder_list = ["data/gt_back_cropped_1/test", "data/gt_back_cropped_2/test", "data/gt_back_cropped_3/test", "data/gt_back_cropped_4/test", "data/gt_back_cropped_5/test", "data/gt_back_cropped_6/test", "data/gt_back_cropped_7/test", "data/gt_back_cropped_8/test", "data/gt_front_cropped_1/test", "data/gt_front_cropped_2/test", "data/gt_front_cropped_3/test"]
-json_folder_list = ["data/gt_back_cropped_1/test/json", "data/gt_back_cropped_2/test/json", "data/gt_back_cropped_3/test/json", "data/gt_back_cropped_4/test/json", "data/gt_back_cropped_5/test/json", "data/gt_back_cropped_6/test/json", "data/gt_back_cropped_7/test/json", "data/gt_back_cropped_8/test/json", "data/gt_front_cropped_1/test/json", "data/gt_front_cropped_2/test/json", "data/gt_front_cropped_3/test/json"]
+json_folder_list = [
+    "data/gt_back_cropped_1/test/json", "data/gt_back_cropped_2/test/json", "data/gt_back_cropped_3/test/json",
+    "data/gt_back_cropped_4/test/json", "data/gt_back_cropped_5/test/json", "data/gt_back_cropped_6/test/json",
+    "data/gt_back_cropped_7/test/json", "data/gt_back_cropped_8/test/json", "data/gt_front_cropped_1/test/json",
+    "data/gt_front_cropped_2/test/json", "data/gt_front_cropped_3/test/json"
+]
 
-for image_folder, json_folder in zip(image_folder_list, json_folder_list):
+for image_folder, json_folder in zip(tqdm(image_folder_list, desc="Processing folders"), json_folder_list):
     output_mask = f"output/{image_folder.split('/')[1]}/mask"
     output_images = f"output/{image_folder.split('/')[1]}/images"
 
-    # os.makedirs(output_mask, exist_ok=True)
-    # os.makedirs(output_images, exist_ok=True)
+    os.makedirs(output_mask, exist_ok=True)
+    os.makedirs(output_images, exist_ok=True)
 
     image_list = os.listdir(image_folder)
-    # json_list = os.listdir(json_folder)
+    json_list = os.listdir(json_folder)
 
     # print("===============================================")
     # print(f"Image folder: {image_folder}")
@@ -25,9 +35,8 @@ for image_folder, json_folder in zip(image_folder_list, json_folder_list):
     # print(f"JSON folder: {json_folder}")
     # print(f"JSON folder: {json_list}")
 
-
-    for filename in image_list:
-        if filename.endswith(".jpg"):
+    for filename in tqdm(image_list, desc=f"Processing images in {image_folder}", leave=False):
+        if filename.endswith(".bmp"):
             image_path = os.path.join(image_folder, filename)
             json_path = os.path.join(json_folder, os.path.splitext(filename)[0] + ".json")
 
